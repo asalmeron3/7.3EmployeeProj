1f8073074df089c149b71d2270d03435a9d0dc08
Arturo Salmeron <asalmeron3@gatech.edu>
2017-09-23 11:10:23 -0400

    Initializing Firebase

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4kAB0zJysihVjCzEwAX5fH-vE9V0K8TM",
+    authDomain: "inclassemployeeassignment.firebaseapp.com",
+    databaseURL: "https://inclassemployeeassignment.firebaseio.com",
+    projectId: "inclassemployeeassignment",
+    storageBucket: "",
+    messagingSenderId: "833622573521"
+  };
+  firebase.initializeApp(config);
